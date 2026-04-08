47a3f4a1151fbfa4d38cc759e935ab2291947967
rizHarism <riz.harism@gmail.com>
2026-04-08 22:11:46 +0700

    fix: vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
+import { map } from "leaflet";
 
 export default defineConfig({
     plugins: [
@@ -8,6 +9,8 @@ export default defineConfig({
                 "resources/css/app.css",
                 "resources/js/app.js",
                 "resources/js/gis/initial-map.js",
+                "resources/js/map/map.js",
+                "resources/js/admin/form-education.",
             ],
             refresh: true,
         }),
